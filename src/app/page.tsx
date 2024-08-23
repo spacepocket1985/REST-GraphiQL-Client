ef48c177d3aacf2beddb954a9e2301282a1ba248
Space Pocket <space1985pocket@gmail.com>
2024-08-23 14:59:56 +0300

    refactor: remove signIn signUp from welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { auth, fetchUserName } from '@/utils/firebase';
-import UIButton from '@/components/ui/UIButton';
 
 import styles from './page.module.css';
+import UIButton from '@/components/ui/UIButton';
 
 export default function WelcomePage() {
   const [user, loading] = useAuthState(auth);
@@ -29,10 +29,35 @@ export default function WelcomePage() {
   const router = useRouter();
   return (
     <>
-      <h2>{name ? `Welcome Back, ${name}!` : 'Welcome!'}</h2>
       <div className={styles.welcomeWrapper}>
-        <UIButton text="Sign In" href="/auth/sign-in" />
-        <UIButton text="Sign Up" href="/auth/sign-up" />
+        <h2>{name ? `Welcome Back, ${name}!` : 'Welcome!'}</h2>
+        {user ? (
+          <>
+            <div className="welcomeSection">
+              <h3 className={styles.wrapperSubTitle}>
+                Available utilities and features
+              </h3>
+              <div className={styles.welcomeTools}>
+                <UIButton text="REST Client" href="" />
+                <UIButton text="GraphiQL Client" href="" />
+                <UIButton text="History" href="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="welcomeSection">
+              <h3 className={styles.wrapperSubTitle}>Info about task</h3>
+              <p>
+                lightweight application that combines the functionalities of two
+                leading API clients: Postman and GraphiQL. It is designed for
+                developers working with both REST and GraphQL APIs, providing an
+                intuitive interface for creating, testing, and documenting
+                requests.
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,21 +10,26 @@ export default function Header() {
   const [user] = useAuthState(auth);
   return (
     <header>
-      <h2>Header</h2>
       <div className={styles.headerWrapper}>
         <UIButton text="Logo" href="" />
-        <UIButton text="Language Toggle" href="" />
-        {user ? (
-          <UIButton
-            text="Sign Out"
-            href={RoutePaths.WELCOME}
-            onClick={logout}
-          />
-        ) : (
-          <UIButton text="Sign In" href={RoutePaths.SIGNIN} />
-        )}
-
-        <UIButton text="Welcome" href={RoutePaths.WELCOME} />
+        <div className={styles.menuWrapper}>
+          <UIButton text="Language Toggle" href="" />
+          {user ? (
+            <>
+              <UIButton
+                text="Sign Out"
+                href={RoutePaths.WELCOME}
+                onClick={logout}
+              />
+            </>
+          ) : (
+            <>
+              <UIButton text="Sign In" href={RoutePaths.SIGNIN} />
+              <UIButton text="Sign Up" href={RoutePaths.SIGNUP} />
+            </>
+          )}
+          <UIButton text="Welcome" href={RoutePaths.WELCOME} />
+        </div>
       </div>
     </header>
   );
