403ba0d20a06834d3d018889d2184997885d00fd
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-08-28 11:29:43 +0300

    feat: update url with encode to base64

--- src/constants/routePaths.ts
+++ src/constants/routePaths.ts
@@ -2,5 +2,5 @@ export enum RoutePaths {
   WELCOME = '/',
   SIGNIN = '/auth/sign-in',
   SIGNUP = '/auth/sign-up',
-  RESTFULL = '/RESTfull-client',
+  RESTFULL = '/RESTfull-client/GET/IA==/e30=/',
 }
